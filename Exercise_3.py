770c1e82e8c54c97b343e6a4b59cae79a324858a
omkar04gaikwad <58172164+omkar04gaikwad@users.noreply.github.com>
2025-05-14 14:38:57 -0700

    Completed Exercise 3 to find middle of Linked List

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,18 +3,34 @@ class Node:
   
     # Function to initialise the node object  
     def __init__(self, data):  
-        
+        self.data = data
+        self.next = None
 class LinkedList: 
   
-    def __init__(self): 
+    def __init__(self):
+        self.head = Node(-1)
         
   
     def push(self, new_data): 
-        
+        curr = self.head.next
+        if curr == None:
+            self.head.next = Node(new_data)
+            return
+        while curr.next:
+            curr = curr.next
+        curr.next = Node(new_data)
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        slow = self.head.next
+        fast = self.head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
+        return
+
 
 # Driver code 
 list1 = LinkedList() 
