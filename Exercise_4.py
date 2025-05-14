9ac0937fd6bfd503a9a5bd8a3ed5b4f779d16f18
omkar04gaikwad <58172164+omkar04gaikwad@users.noreply.github.com>
2025-05-14 14:39:21 -0700

    Completed Exercise 4 to implement Merge Sort

--- Exercise_4.py
+++ Exercise_4.py
@@ -2,11 +2,41 @@
 def mergeSort(arr):
   
   #write your code here
+  if len(arr) > 1:
+    mid = len(arr)//2
+    L = arr[:mid]
+    R = arr[mid:]
+    mergeSort(L)
+    mergeSort(R)
+    i,j,k  = 0,0,0
+    while i < len(L) and j < len(R):
+      if L[i] < R[j]:
+        arr[k] = L[i]
+        i += 1
+      else:
+        arr[k] = R[j]
+        j += 1
+      k += 1
+    
+    while i < len(L):
+      arr[k] = L[i]
+      i += 1
+      k += 1
+    
+    while j < len(R):
+      arr[k] = R[j]
+      j += 1
+      k += 1
+      
   
 # Code to print the list 
-def printList(arr): 
+def printList(arr):
+
     
     #write your code here
+      n = len(arr)
+      for i in range(n): 
+        print ("%d" %arr[i]), 
   
 # driver code to test the above code 
 if __name__ == '__main__': 
